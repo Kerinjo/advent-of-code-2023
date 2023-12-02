2cb525ea89b070d14db2dd9bdc89e0b50c64c6f0
Kerinjo <konmarciniak@gmail.com>
2023-12-02 22:44:05 +0100

    day 1 part 1

---
+++ day-1/Program.cs
@@ -0,0 +1,27 @@
+﻿static int Solve()
+{
+    string filePath = "input/puzzle-input.txt";
+
+    try
+    {
+        string[] lines = File.ReadAllLines(filePath);
+
+        int sum = 0;
+        foreach (string line in lines)
+        {
+            char firstNumeric = line.FirstOrDefault(char.IsDigit);
+            char lastNumeric = line.LastOrDefault(char.IsDigit);
+            string twoDigit = firstNumeric.ToString() + lastNumeric;
+            sum += Convert.ToInt32(twoDigit);
+        }
+        return sum;
+    }
+    catch (Exception ex)
+    {
+        System.Console.WriteLine($"Error: {ex.Message}");
+        return 0;
+    }
+}
+
+int result = Solve();
+System.Console.WriteLine(result);
